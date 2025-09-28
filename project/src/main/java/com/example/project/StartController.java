8aaab5327c658bb06733cfb9862b4bea56894f7a
bogdanvujanac <vujanacbogdan@gmail.com>
2025-09-28 13:30:49 +0200

    Created start scene - GUI

---
+++ project/src/main/java/com/example/project/StartController.java
@@ -0,0 +1,13 @@
+package com.example.project;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class StartController {
+
+    @FXML
+    private Button btn_start;
+
+    @FXML
+    private Button btn_exit;
+}
